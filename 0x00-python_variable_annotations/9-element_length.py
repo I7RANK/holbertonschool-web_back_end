aee305cc7cbf931b63a0f2a5ba523ee490d317f3
I7RANK <franfgh2000@hotmail.com>
2021-07-19 20:07:31 -0500

    Uploading advanced tasks and fixing task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""This module contains the function make_multiplier
+"""This module contains the function element_length
 """
 
 
@@ -7,4 +7,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """only a function that make something :v"""
     return [(i, len(i)) for i in lst]
