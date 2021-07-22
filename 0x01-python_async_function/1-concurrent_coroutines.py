403663e2e345bb553b07e3ee372bcee60d281f91
I7RANK <franfgh2000@hotmail.com>
2021-07-21 22:49:50 -0500

    Fixing tasks 1 and 4 using asyncio.as_completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,8 +11,13 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """waits for a random delay between 0 and max_delay"""
     new_list: List[float] = []
+    new_list2: List[float] = []
 
     for i in range(n):
-        new_list.append(await wait_random(max_delay))
+        new_list.append(wait_random(max_delay))
 
-    return new_list
+    for i in asyncio.as_completed(new_list):
+        num: float = await i
+        new_list2.append(num)
+
+    return new_list2
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,9 +11,13 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """waits for a random delay between 0 and max_delay"""
     new_list: List[float] = []
+    new_list2: List[float] = []
 
     for i in range(n):
-        num: float = await task_wait_random(max_delay)
-        new_list.append(num)
+        new_list.append(task_wait_random(max_delay))
 
-    return new_list
+    for i in asyncio.as_completed(new_list):
+        num: float = await i
+        new_list2.append(num)
+
+    return new_list2
