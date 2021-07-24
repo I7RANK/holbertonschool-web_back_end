25064401758e7bd8774795ed3272551dffbc023d
I7RANK <franfgh2000@hotmail.com>
2021-07-24 14:46:22 -0500

    Fixing task 0 Generator syntax

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator, AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """yields a random number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
