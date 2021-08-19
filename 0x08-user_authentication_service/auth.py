191e31f975b919c01bdae925aed66bf8b47313d0
I7RANK <franfgh2000@hotmail.com>
2021-08-18 21:40:01 -0500

    Fixing annotation in task 13

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -92,14 +92,10 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """updates the corresponding user’s session ID to None
         """
-        user = None
-
         try:
-            user = self._db.find_user_by(user_id=user_id)
+            self._db.update_user(user_id, session_id=None)
         except NoResultFound:
-            pass
-
-        setattr(user, "session_id", None)
+            return None
 
     def get_reset_password_token(self, email: str) -> str:
         """generates a token to restore password if the user exist
