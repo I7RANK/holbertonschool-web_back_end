3ecf324e97e2c7b8e26fde6860f0eaf9d0f484c1
I7RANK <franfgh2000@hotmail.com>
2021-08-22 15:06:02 -0500

    Uploading task 1

---
+++ 0x09-Unittests_and_integration_tests/README.md
@@ -0,0 +1,8 @@
+# 0x09. Unittests and Integration Tests
+[Link to the project](https://intranet.hbtn.io/projects/610)
+
+## Learning Objectives
+<ul>
+<li>The difference between unit and integration tests.</li>
+<li>Common testing patterns such as mocking, parametrizations and fixtures</li>
+</ul>
---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This module contais the test for the utils class
+"""
+
+import unittest
+from parameterized import parameterized, parameterized_class
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """test class for utils.access_nested_map method"""
+    @parameterized.expand([
+        ('expected_1', {"a": 1}, ("a",), 2),
+        ('expected_dict', {"a": {"b": 2}}, ("a",), 2),
+        ('expected_2', {"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, name, nested_map, path, expected):
+        """test for utils.access_nested_map method"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
